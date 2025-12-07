c4f2feee6ca9650eebae0af38a5f4d9b75ddb050
mirotol <mirotolkkila@gmail.com>
2025-12-07 23:37:33 +0200

    Add Altimeter instrument

--- frontend/src/components/dashboard/Dashboard.tsx
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import AttitudeIndicator from '../AttitudeIndicator/AttitudeIndicator';
+import AttitudeIndicator from '../attitudeindicator/AttitudeIndicator';
+import Altimeter from '../altimeter/Altimeter';
 
 export default function Dashboard() {
   return (
@@ -7,12 +8,13 @@ export default function Dashboard() {
       style={{
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'flex-end',
         width: '100%',
         height: '100%',
       }}
     >
       <AttitudeIndicator width="120%" height="120%" />
+      <Altimeter width="120%" height="120%" />
     </div>
   );
 }
