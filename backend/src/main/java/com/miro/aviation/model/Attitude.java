fa758a62652ae84025ee9b55f57b4cc2917cf57a
mirotol <mirotolkkila@gmail.com>
2025-12-07 22:29:25 +0200

    Update Attitude comments

--- backend/src/main/java/com/miro/aviation/model/Attitude.java
+++ backend/src/main/java/com/miro/aviation/model/Attitude.java
@@ -3,9 +3,9 @@ package com.miro.aviation.model;
 import java.time.Instant;
 
 public class Attitude {
-    private double pitch; // degrees
-    private double roll;  // degrees
-    private double yaw;
+    private double pitch; // degrees (nose up/down)
+    private double roll;  // degrees (bank/tilt left or right)
+    private double yaw; // degrees (heading, 360 max value)
     private Instant timestamp;
 
     public Attitude(double pitch, double roll, double yaw) {
