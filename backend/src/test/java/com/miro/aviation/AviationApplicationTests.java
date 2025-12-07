08e424be7dc5ca35d34c4d97043d3b53883ff3af
mirotol <mirotolkkila@gmail.com>
2025-12-07 21:37:42 +0200

    Init repository

---
+++ backend/src/test/java/com/miro/aviation/AviationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.aviation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
