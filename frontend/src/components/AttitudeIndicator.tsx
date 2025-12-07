1ccf6082058ce47dcb3ee571e4b8d02de0889e70
mirotol <mirotolkkila@gmail.com>
2025-12-07 22:38:47 +0200

    Move websocket logic to hook

--- frontend/src/components/AttitudeIndicator.tsx
+++ frontend/src/components/AttitudeIndicator.tsx
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Client } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
+import React from 'react';
+import { useAttitude } from '../hooks/useAttitude';
 
 export default function AttitudeIndicator() {
-  const [attitude, setAttitude] = useState({ pitch: 0, roll: 0, yaw: 0 });
-
-  useEffect(() => {
-    const socket = new SockJS('http://localhost:8080/ws/attitude');
-    const stompClient = new Client({
-      webSocketFactory: () => socket
-    });
-
-    stompClient.onConnect = () => {
-      stompClient.subscribe('/topic/attitude', (message) => {
-        const data = JSON.parse(message.body);
-        setAttitude(data);
-      });
-    };
-
-    stompClient.activate();
-
-    return () => {
-      stompClient.deactivate();
-    };
-  }, []);
+  const attitude = useAttitude();
 
   return (
     <div style={{ textAlign: 'center', fontFamily: 'monospace' }}>
---
+++ frontend/src/hooks/useAttitude.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+
+export interface Attitude {
+  pitch: number;
+  roll: number;
+  yaw: number;
+}
+
+export function useAttitude() {
+  const [attitude, setAttitude] = useState<Attitude>({ pitch: 0, roll: 0, yaw: 0 });
+
+  useEffect(() => {
+    const socket = new SockJS('http://localhost:8080/ws/attitude');
+    const stompClient = new Client({
+      webSocketFactory: () => socket
+    });
+
+    stompClient.onConnect = () => {
+      stompClient.subscribe('/topic/attitude', (message) => {
+        const data: Attitude = JSON.parse(message.body);
+        setAttitude(data);
+      });
+    };
+
+    stompClient.activate();
+
+    return () => {
+      stompClient.deactivate();
+    };
+  }, []);
+
+  return attitude;
+}
